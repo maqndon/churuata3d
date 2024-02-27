1adac8d50b40a0a295b941b25f82aa83ce453875
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2024-02-27 16:15:27 +0100

    refactor

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -28,7 +28,56 @@ class BaseController extends Controller
         $this->productCommonContent = $productCommonContent;
     }
 
-    protected function showCommon(Request $request, $model, $slug, $relation)
+    protected function loadCommonData()
+    {
+        $mostDownloadedProducts = $this->mostDownloaded(3);
+        return [
+            'mostDownloadedProducts' => $mostDownloadedProducts,
+        ];
+    }
+
+    protected function getProductData(Request $request, $slug, $slug2, $relation)
+    {
+        try {
+            $product = Product::with(['images', 'licence', 'tags', 'categories', 'print_settings', 'files'])
+                ->where('slug', $slug2)
+                ->where('status', 'published')
+                ->whereRelation($relation, 'slug', $slug)
+                ->firstOrFail();
+
+            $commonContent = $this->productCommonContent->getProductCommonContent();
+            $totalImages = collect($product->images->images_names)->count();
+            $relatedParametric = $this->productService->getParametric($slug2);
+            $data = $this->loadCommonData();
+            $billOfMaterials = $this->getBillOfMaterials(Product::class, $product->id);
+            $relatedProducts = $this->productService->getRelatedProducts($product);
+
+            return compact(
+                'product',
+                'totalImages',
+                'commonContent',
+                'billOfMaterials',
+                'relatedProducts',
+                'relatedParametric',
+                'relation'
+            );
+        } catch (\Throwable $th) {
+            abort(404);
+        }
+    }
+
+    public function showProduct(Request $request, $slug, $slug2, $relation)
+    {
+        $data = $this->getProductData($request, $slug, $slug2, $relation);
+        $data['slug'] = $slug;
+
+        $slugType = $relation === 'categories' ? 'category_slug' : 'tag_slug';
+        $data[$slugType] = $slug;
+
+        return view('products.show', $data);
+    }
+
+    protected function getModelData(Request $request, $model, $slug, $relation)
     {
         $item = $model::where('slug', $slug)->firstOrFail();
         $products = Product::with($relation)
@@ -36,129 +85,15 @@ class BaseController extends Controller
             ->get();
 
         $data = $this->loadCommonData();
-
-        $slugTypeData = $this->getRelationTypeAndSlug($relation, $slug, $products, $item);
-
-        // data array
-        $compactData = $slugTypeData + $data;
-
-        // final view
-        return view($this->getViewType($relation), $compactData);
-    }
-
-    protected function getViewType($relation)
-    {
-        // view type
-        $viewType = $relation === 'categories' ? 'categories.show' : 'tags.show';
-
-        return $viewType;
-    }
-
-    protected function getRelationTypeAndSlug($relation, $slug, $products, $item)
-    {
-
-        // slug type
         $slugType = $relation === 'categories' ? 'category_slug' : 'tag_slug';
-
-        // slug value
         ${$slugType} = $slug;
 
-        if ($slugType === 'category_slug') {
-            return compact(
-                'products',
-                'item',
-                'category_slug'
-            );
-        } elseif ($slugType === 'tag_slug') {
-            return compact(
-                'products',
-                'item',
-                'tag_slug'
-            );
-        }
+        return compact('products', 'item', $slugType) + $data;
     }
 
-    public function showProduct(Request $request, $slug, $slug2, $relation)
+    public function showCommon(Request $request, $model, $slug, $relation)
     {
-
-        try {
-            //product
-            $product = Product::with(['images', 'licence', 'tags', 'categories', 'print_settings', 'files'])
-                ->where('slug', $slug2)
-                ->where('status', 'published')
-                ->whereRelation($relation, 'slug', $slug)
-                ->first();
-
-            //product common contents
-            $commonContent =  $this->productCommonContent->getProductCommonContent();
-
-            // Count the number of images associated with the product
-            $totalImages = collect($product->images->images_names)->count();
-
-            //related parametric product
-            $relatedParametric = $this->productService->getParametric($slug2);
-
-            //most downloaded products
-            $data = $this->loadCommonData();
-            // $mostDownloadedProducts = $this->mostDownloaded(3);
-
-            //bill of materials
-            $billOfMaterials = $this->getBillOfMaterials(Product::class, $product->id);
-
-            //related products
-            $relatedProducts = $this->productService->getRelatedProducts($product);
-
-            switch ($relation) {
-                case 'categories':
-                    $category_slug = $slug;
-                    return view('products.show', compact(
-                        'category_slug',
-                        'product',
-                        'totalImages',
-                        'commonContent',
-                        'billOfMaterials',
-                        'relatedProducts',
-                        'relatedParametric',
-                        'relation'
-                    ) + $data);
-                    break;
-                case 'tags':
-                    $tag_slug = $slug;
-                    return view('products.show', compact(
-                        'tag_slug',
-                        'product',
-                        'totalImages',
-                        'commonContent',
-                        'billOfMaterials',
-                        'relatedProducts',
-                        'relatedParametric',
-                        'relation'
-                    ) + $data);
-                    break;
-            }
-        } catch (\Throwable $th) {
-            abort(404);
-        }
-    }
-
-    protected function loadCommonData()
-    {
-        $mostDownloadedProducts = $this->mostDownloaded(3);
-
-        return [
-            'mostDownloadedProducts' => $mostDownloadedProducts,
-        ];
-    }
-
-    protected function getRelation($relation, $slug)
-    {
-        switch ($relation) {
-            case 'categories':
-                return $category_slug = $slug;
-                break;
-            case 'tags':
-                return $tag_slug = $slug;
-                break;
-        }
+        $data = $this->getModelData($request, $model, $slug, $relation);
+        return view($relation . '.show', $data);
     }
 }
