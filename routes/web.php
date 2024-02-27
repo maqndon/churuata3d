b93a090e216f1497708ef5a1685edd2e8dcd904c
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2024-02-27 11:36:32 +0100

    site structured changed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
@@ -16,19 +17,33 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', [HomeController::class, 'show'])
     ->name(('welcome'));
 
-Route::prefix('products')->group(function () {
-    Route::get('{category_slug}/', [ProductController::class, 'indexCategory'])
-        ->name('products.indexCategory');
+Route::prefix('products/category')->group(function () {
+    Route::get('{category_slug}/', [CategoryController::class, 'show'])
+        ->name('categories.show');
     Route::get('{category_slug}/{product_slug}', [ProductController::class, 'show'])
-        ->name('products.show');
+        ->name('products.showByCategory');
 });
 
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{category_slug}/{product_slug}/download-files', [ProductController::class, 'downloadFiles']);
+Route::get('/products/{product_slug}/download-files', [ProductController::class, 'downloadFiles'])
+    ->name('products.downloadFiles');
+
+Route::prefix('products')->group(function () {
+    Route::get('categories', [ProductController::class, 'index'])
+        ->name('products.index');
+    Route::get('/', [ProductController::class, 'index'])
+        ->name('products.index');
+});
+
+Route::prefix('products')->group(function () {
+    Route::get('tags', [TagController::class, 'index'])
+        ->name('tags.index');
+    Route::get('tag/{tag_slug}', [TagController::class, 'show'])
+        ->name('tags.show');
+    Route::get('{tag_slug}/{product_slug}', [ProductController::class, 'show'])
+        ->name('products.showByTag');
+});
+
