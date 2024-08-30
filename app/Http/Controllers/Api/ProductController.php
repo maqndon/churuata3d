0c2880b751ceeb2507b5ebf6570b3426ab650de1
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2024-08-30 16:37:57 +0200

    seo, categories, tags, etc added

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -53,6 +53,10 @@ class ProductController extends Controller
             'related_parametric' => 'string',
             'downloads' => 'numeric',
             'created_at' => 'required|date',
+            'printing_materials' => 'array',
+            'printing_materials.*.id' => 'numeric',
+            'print_settings' => 'array',
+            'print_settings.*.id' => 'numeric',
             'bill_of_materials' => 'array',
             'bill_of_materials.*.qty' => 'integer',
             'bill_of_materials.*.item' => 'string|max:255',
@@ -60,6 +64,18 @@ class ProductController extends Controller
             'files.*.files_names' => 'array',
             'files.*.files_names.*' => 'string',
             'files.*.metadata' => 'string|max:255',
+            'images' => 'array',
+            'images.*.images_names' => 'array',
+            'images.*.images_names.*' => 'string',
+            'images.*.metadata' => 'string|max:255',
+            'seos' => 'array',
+            'seos.*.title' => 'string|unique:seos,title',
+            'seos.*.meta_description.*' => 'string',
+            'seos.*.meta_keywords' => 'array',
+            'categories' => 'array',
+            'categories.*.category_id' => 'numeric',
+            'tags' => 'array',
+            'tags.*.tag_id' => 'numeric'
         ]);
 
         try {
@@ -84,31 +100,73 @@ class ProductController extends Controller
                 'downloads' => $request->input('downloads'),
                 'created_at' => $request->input('created_at'),
             ]);
-    
+
+            // add printing materials
+            if ($request->printing_materials) {
+                foreach ($request->input('print_settings') as $print_setting) {
+                    $product->print_settings()->attach($print_setting);
+                }
+            }
+
+            // add print settings
+            if ($request->printing_materials) {
+                foreach ($request->input('printing_materials') as $printing_material) {
+                    $product->printing_materials()->attach($printing_material);
+                }
+            }
+
             // add bill of materials
             if ($request->bill_of_materials) {
                 foreach ($request->input('bill_of_materials') as $bom) {
                     $product->bill_of_materials()->create($bom);
                 }
             }
-    
+
             // add product files
             if ($request->input('files')) {
                 foreach ($request->input('files') as $fileData) {
                     $product->files()->create($fileData);
                 }
             }
-    
+
+            // add product images
+            if ($request->input('images')) {
+                foreach ($request->input('images') as $image) {
+                    $product->images()->create($image);
+                }
+            }
+
+            // add product seo
+            if ($request->input('seos')) {
+                foreach ($request->input('seos') as $seo) {
+                    $product->seos()->create($seo);
+                }
+            }
+
+            // add product categories
+            if ($request->input('categories')) {
+                foreach ($request->input('categories') as $category) {
+                    $product->categories()->sync($category);
+                }
+            }
+
+            // add product tags
+            if ($request->input('tags')) {
+                foreach ($request->input('categories') as $tag) {
+                    $product->tags()->sync($tag);
+                }
+            }
+
             return response()->json([
                 'message' => 'Product ' . $request->title . ' Created Successfully',
             ], 201);
         } catch (\Throwable $th) {
-            #\Log::error('Error creating product: ' . $th->getMessage());
+            \Log::error('Error creating product: ' . $th->getMessage());
+
             return response()->json([
                 'message' => 'Product could not be Created Successfully',
             ], 500);
         }
-
     }
 
     /**
