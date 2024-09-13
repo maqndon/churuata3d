74a2e41cbd6caf1ddb17272c5a30f5a0b0fe6afd
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2024-09-13 09:00:15 +0200

    error fixed

--- app/Http/Requests/Categories/StoreCategoryRequest.php
+++ app/Http/Requests/Categories/StoreCategoryRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Tags;
+namespace App\Http\Requests\Categories;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreTagRequest extends FormRequest
+class StoreCategoryRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,8 +22,8 @@ class StoreTagRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|unique:tags,name',
-            'slug' => 'required|alpha_dash|unique:tags,slug',
+            'name' => 'required|string|unique:categories,name',
+            'slug' => 'required|alpha_dash|unique:categories,slug',
         ];
     }
 }
