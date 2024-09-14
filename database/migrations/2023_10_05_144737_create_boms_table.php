1f5ea9b9e3a6a183f008880578baa8971bd96dfc
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2024-09-14 11:56:46 +0200

    migration improvement by adding unique method

--- database/migrations/2023_10_05_144737_create_boms_table.php
+++ database/migrations/2023_10_05_144737_create_boms_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('qty');
             $table->string('item');
             $table->timestamps();
+            $table->unique(['bomable_type', 'bomable_id', 'item']);
         });
     }
 
